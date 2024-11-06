5bec1e9e6ae9e5fdc61163384781523fd5d99923
dasdo <pamela.navia@ucb.edu.bo>
2024-11-06 17:39:45 -0400

    crud materia completo

--- routes/materias.js
+++ routes/materias.js
@@ -1,9 +1,13 @@
-// routes/materias.js
+// routes/materiaRoutes.js
 const express = require('express');
 const router = express.Router();
-const MateriaController = require('../controllers/materiaController');
+const MateriaController = require('../controllers/MateriaController');
 
-router.post('/', MateriaController.crearMateria);
-router.get('/', MateriaController.obtenerMaterias);
+// Rutas para el CRUD de Materia
+router.post('/', MateriaController.crearMateria); // Crear materia
+router.get('/', MateriaController.obtenerMaterias); // Obtener todas las materias
+router.get('/:idMateria', MateriaController.obtenerMateriaPorId); // Obtener materia por ID
+router.put('/:idMateria', MateriaController.actualizarMateria); // Actualizar materia
+router.delete('/:idMateria', MateriaController.eliminarMateria); // Eliminar materia
 
 module.exports = router;
