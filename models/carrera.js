fce2a659ca5c648ace556fa9dfee4c7ef2e4014d
dasdo <pamela.navia@ucb.edu.bo>
2024-11-06 17:34:49 -0400

    crud carrera completo

--- models/carrera.js
+++ models/carrera.js
@@ -4,10 +4,14 @@ const db = require('../config/db');
 const Carrera = {
   crearCarrera: (data, callback) => {
     const query = `
-      INSERT INTO carrera (idCarrera, nombreCarrera, modalidadIngreso)
+      INSERT INTO carreras (idCarrera, nombreCarrera, modalidadIngreso)
       VALUES (?, ?, ?)`;
       
-    db.query(query, [data.idCarrera, data.nombreCarrera, data.modalidadIngreso], (err, results) => {
+    db.query(query, [
+      data.idCarrera,
+      data.nombreCarrera,
+      data.modalidadIngreso
+    ], (err, results) => {
       if (err) {
         console.error('Error al crear carrera:', err);
         return callback(err);
@@ -17,10 +21,52 @@ const Carrera = {
   },
   
   obtenerCarreras: (callback) => {
-    const query = 'SELECT * FROM carrera';
+    const query = 'SELECT * FROM carreras';
     db.query(query, (err, results) => {
       if (err) {
         console.error('Error al obtener carreras:', err);
+        return callback(err);
+      }
+      callback(null, results);
+    });
+  },
+
+  obtenerCarreraPorId: (idCarrera, callback) => {
+    const query = 'SELECT * FROM carreras WHERE idCarrera = ?';
+    db.query(query, [idCarrera], (err, results) => {
+      if (err) {
+        console.error('Error al obtener carrera:', err);
+        return callback(err);
+      }
+      callback(null, results[0]);
+    });
+  },
+
+  actualizarCarrera: (idCarrera, data, callback) => {
+    const query = `
+      UPDATE carreras SET 
+        nombreCarrera = ?, 
+        modalidadIngreso = ?
+      WHERE idCarrera = ?`;
+      
+    db.query(query, [
+      data.nombreCarrera,
+      data.modalidadIngreso,
+      idCarrera
+    ], (err, results) => {
+      if (err) {
+        console.error('Error al actualizar carrera:', err);
+        return callback(err);
+      }
+      callback(null, results);
+    });
+  },
+
+  eliminarCarrera: (idCarrera, callback) => {
+    const query = 'DELETE FROM carreras WHERE idCarrera = ?';
+    db.query(query, [idCarrera], (err, results) => {
+      if (err) {
+        console.error('Error al eliminar carrera:', err);
         return callback(err);
       }
       callback(null, results);
--- routes/carrera.js
+++ routes/carrera.js
@@ -1,9 +1,21 @@
-// routes/carreras.js
+// routes/carrera.js
 const express = require('express');
 const router = express.Router();
 const CarreraController = require('../controllers/carreraController');
 
+// Crear una nueva carrera
 router.post('/', CarreraController.crearCarrera);
+
+// Obtener todas las carreras
 router.get('/', CarreraController.obtenerCarreras);
+
+// Obtener una carrera por ID
+router.get('/:idCarrera', CarreraController.obtenerCarreraPorId);
+
+// Actualizar una carrera por ID
+router.put('/:idCarrera', CarreraController.actualizarCarrera);
+
+// Eliminar una carrera por ID
+router.delete('/:idCarrera', CarreraController.eliminarCarrera);
 
 module.exports = router;
